7c66db1eb5c5a69c5a322bad1b4ad1d6afc66315
Shaiishken <bekarys_kenjebek@icloud.com>
2026-05-18 02:28:02 +0500

    Revert "Add Restaurant & Catalog Service"

--- migrations/002_create_restaurants.up.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE restaurants (
-    id SERIAL PRIMARY KEY,
-    category_id INT REFERENCES categories(id),
-    name VARCHAR(255) NOT NULL,
-    description TEXT,
-    rating FLOAT DEFAULT 0,
-    open_time TIME,
-    close_time TIME
-);
